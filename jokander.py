7ddc09ea11f96196e635b84c4678c7d8f7a1f790
Axel Chaves <79960066+AxelCHCR@users.noreply.github.com>
2022-11-01 00:35:57 -0600

    Board Updates
    Generated board and user interaction in process

--- jokander.py
+++ jokander.py
@@ -1,4 +1,35 @@
+import time as t
 import random as r
+def mostrarInfoJuego():
+    print("\n¡Bienvenido a Jokander: El juego Final!")
+    print(f"El juego consiste en tomar una casilla del tablero e ir encontrando puntos alrededor de él.\n\
+Se tiene un número aleatorio de intentos para ganar el juego, el cual se gana con las siguientes condiciones:\n\
+    - Obteniendo un 50% de los puntos positivos del tablero\n\
+    - Encontrando el territorio emergente del trono Jokander.")
+    print("\n¡Ten cuidado! También se puede perder el juego si:\n\
+    - Se obtienen 50 puntos negativos\n\
+    - Se encuentra el territorio emergente perdedor\n\
+    - Se acaban los intentos")
+def buscarCasilla(tablero, casilla):
+    fila = int(casilla[0])
+    columna = int(casilla[2:])
+    print(f"La casila encontrada contiene al número {tablero[fila-1][columna-1]}")
+    return (tablero[fila][columna], fila, columna)
+
+
+
+def imprimirTablero(tablero):
+    visualizacion = ""
+    visualizacion += "\t\t"
+    for i in range(1, len(tablero)+1):
+        trozoTablero =f"{i} |\t"
+        visualizacion += trozoTablero
+    visualizacion += "\n"
+    for i in range(1, len(tablero)+1):
+        trozoTablero = f"___\n{i}\n"
+        visualizacion += trozoTablero
+    return visualizacion
+
 def generarTablero():
     tamano = r.randint(8,10)
     matriz = []
@@ -71,7 +102,42 @@ def generarTablero():
             break
     return matriz
 matriz = generarTablero()
-for i in matriz:
-    print(i)
+"""for i in matriz:
+    print(i)"""
+
+def juego():
+    mostrarInfoJuego()
+    intentos = r.randint(12,18)
+    casillasJugadas = []
+    nombre = input("Ingrese su nombre: ")
+    print(f"{nombre}, para esta partida tienes {intentos} intentos.")
+    matriz = generarTablero()
+    print("La casilla se solicita usando el siguiente formato:\n\
+            Fila-Columna. Ejemplo: 1-2.\n")
+    print(imprimirTablero(matriz))
+    contadorIntentos = 0
+    puntosPositivos = 0
+    puntosNegativos = 0
+    while contadorIntentos < intentos:
+        casilla = input("Ingrese la casilla a buscar: ")
+        casillaAMostrar = buscarCasilla(matriz, casilla)
+        casillasJugadas.append((casillaAMostrar[1], casillaAMostrar[2]))
+        if casillaAMostrar[0] == 5:
+            print(f"{nombre} ha ganado 5 puntos positivos. ")
+        if casillaAMostrar[0] == -5:
+            print(f"{nombre} ha ganado 5 puntos negativos. ")
+        if casillaAMostrar[0] == 10:
+            print(f"{nombre} ha ganado 10 puntos positivos. ")
+        if casillaAMostrar[0] == -10:
+            print(f"{nombre} ha ganado 10 puntos negativos. ")
+        if casillaAMostrar[0] == 100:
+            print(f"¡{nombre} ha ganado el juego!")
+        if casillaAMostrar[0] == -100:
+            print(f"¡{nombre} ha ganado el juego!")
+        
+        print(casillasJugadas)
+        contadorIntentos+=1
+
 def main():
-    pass
+    juego()
+main()
